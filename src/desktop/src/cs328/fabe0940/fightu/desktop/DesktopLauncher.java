f9904b88da3ca4250094750cb3c40ddd56ddcb52
fabe0940 <fabe0940@vandals.uidaho.edu>
2015-04-14 17:53:49 -0700

    created libgdx + kryonet project

---
+++ src/desktop/src/cs328/fabe0940/fightu/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package cs328.fabe0940.fightu.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import cs328.fabe0940.fightu.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
